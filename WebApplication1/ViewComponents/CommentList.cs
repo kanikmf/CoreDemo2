57132376f0cadac4d8aca82055918e0c14baa140
fatih <fatih@DESKTOP-792A8RT>
2023-10-24 15:30:45 +0300

    yorumlar kısmı düzeltildi

---
+++ WebApplication1/ViewComponents/CommentList.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentvalues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID = 1,
+					Username = "Murat"
+				},
+				new UserComment
+				{
+					ID = 2,
+					Username = "Mesut"
+				},
+				new UserComment
+				{
+					ID = 3,
+					Username = "Merve"
+				}
+			};
+			return View(commentvalues);
+		}
+	}
+}
